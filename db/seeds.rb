3e200c0fe70e38fe207c6100bee74fea66e325d6
Nikita <nikita.arar@gmail.com>
2019-01-16 11:10:25 +0200

    heroku

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,3 +6,4 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 AdminUser.create!(email: 'admin@example.com', password: 'password', password_confirmation: 'password') if Rails.env.development?
+AdminUser.create!(email: 'admin@example.com', password: 'BHDFn4wegf4y8wbvsdh', password_confirmation: 'BHDFn4wegf4y8wbvsdh') if Rails.env.production?
